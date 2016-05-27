de80c6d001adc2122539bac872fdd26feb00be24
smilesound <josegonsaa1234@gmail.com>
2016-05-27 09:17:03 -0600

    solucion de los problemas 2,3,4

--- proyecto1.py
+++ proyecto1.py
@@ -1,3 +1,19 @@
-def par (n):
-	if n%2:
-		return "es par"
+def prob_1 (n):
+	return n%2==0
+
+def prob_2 (n):
+	b=n-32/1.8
+	return b 
+
+def prob_3(b,p):
+	p=b
+	for i in range (1,p):
+		b=b*p
+	return b
+
+def prob_4(c,l):
+	h=len(c)
+	li=l-h
+	li=li//2
+	ld=l-li
+	return ("*"*li, c , )
